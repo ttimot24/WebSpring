afe848b982decc2ed67f0a1e88fe1bea6c10ce2e
Timot Tarjani <ttimotster@gmail.com>
2018-07-15 15:45:31 +0200

    Starter commit with sample CircleCi config

---
+++ src/java/Application.java
@@ -0,0 +1,44 @@
+
+import config.AppConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+import org.hibernate.cfg.Environment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class Application implements WebApplicationInitializer {
+
+    @Autowired
+    Environment environment;
+    
+    @Override
+    public void onStartup(ServletContext container) {
+        
+        AnnotationConfigWebApplicationContext application = new AnnotationConfigWebApplicationContext();
+        application.register(AppConfig.class);
+        
+        container.addListener(new ContextLoaderListener(application));
+        
+         ServletRegistration.Dynamic dispatcher = container
+          .addServlet("dispatcher", new DispatcherServlet(application));
+         
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+        
+    }
+    
+
+}
---
+++ src/java/config/AppConfig.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+/**
+ *
+ * @author user
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("controller")
+public class AppConfig extends WebMvcConfigurerAdapter{
+    
+    @Bean
+    public UrlBasedViewResolver getViewResovler() {
+        UrlBasedViewResolver urlBasedViewResolver = new UrlBasedViewResolver();
+        urlBasedViewResolver.setViewClass(JstlView.class);
+        urlBasedViewResolver.setPrefix("/WEB-INF/jsp/");
+        urlBasedViewResolver.setSuffix(".jsp");
+        return urlBasedViewResolver;
+    }
+    
+}
