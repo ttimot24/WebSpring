fcc3a61d878171609ffc762e2fbd537b41460b13
Timot Tarjani <ttimotster@gmail.com>
2018-07-17 23:55:30 +0200

    fixing migration issues

--- src/main/java/com/tarjani/timot/webspring/config/AppConfig.java
+++ src/main/java/com/tarjani/timot/webspring/config/AppConfig.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -21,8 +23,8 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan("controller")
-public class AppConfig extends WebMvcConfigurerAdapter{
+@ComponentScan("com.tarjani.timot.webspring.controller")
+public class AppConfig implements WebMvcConfigurer{
     
     @Bean
     public UrlBasedViewResolver getViewResovler() {
@@ -31,6 +33,12 @@ public class AppConfig extends WebMvcConfigurerAdapter{
         urlBasedViewResolver.setPrefix("/WEB-INF/jsp/");
         urlBasedViewResolver.setSuffix(".jsp");
         return urlBasedViewResolver;
+    }
+    
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
     
 }
