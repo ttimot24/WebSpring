8e82e33759b9d33a80404b4d771b3cd230798b29
Timot Tarjani <ttimotster@gmail.com>
2019-01-12 14:37:48 +0100

    Authentication config file added

---
+++ src/main/java/com/tarjani/timot/webspring/config/AuthConfig.java
@@ -0,0 +1,46 @@
+package com.tarjani.timot.webspring.config;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ *
+ * @author user
+ */
+@Configuration
+@EnableWebSecurity
+public class AuthConfig extends WebSecurityConfigurerAdapter{
+    
+    @Autowired
+    private DataSource dataSource;
+    
+    @Override
+    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(this.dataSource)
+                .passwordEncoder(this.passwordEncoder())
+                .usersByUsernameQuery("TODO");
+    }
+ 
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+          .csrf().disable()
+          .authorizeRequests()
+          .anyRequest().authenticated();
+    }
+    
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+}
