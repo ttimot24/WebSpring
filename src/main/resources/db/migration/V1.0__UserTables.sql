eb860a253a54dbdd42f2d66961ec73fe7e3ea328
Timot Tarjani <ttimotster@gmail.com>
2019-02-17 14:00:38 +0100

    Added user_roles table to the migration file

--- src/main/resources/db/migration/V1.0__UserTables.sql
+++ src/main/resources/db/migration/V1.0__UserTables.sql
@@ -2,7 +2,25 @@
  * Author:  Timot Tarjani
  * Created: 2018.12.23.
  */
-
+CREATE TABLE IF NOT EXISTS `user_roles` (
+  `id` int(10) unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `permission` int(11) NOT NULL,
+  `rights` text
+) AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+--
+    INSERT INTO `user_roles` (`id`, `name`, `permission`, `rights`) VALUES
+    (1, 'Public', 0, NULL),
+    (2, 'User', 1, NULL),
+    (3, 'Member', 2, NULL),
+    (4, 'Editor', 3, '["admin_area","blogpost","page","media"]'),
+    (5, 'Manager', 4, '["admin_area","blogpostcategory","blogpostcomment","blogpost","page","search","user"]'),
+    (6, 'Admin', 5, '["admin_area","blogpostcategory","blogpostcomment","blogpost","dashboard","filemanager","headerimage","page","plugin","search","settings","theme","user","userrole"]');
+    --
+    --
+    ALTER TABLE `user_roles`
+        ADD PRIMARY KEY (`id`);
+    --
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL,
   `name` varchar(255) NOT NULL,
