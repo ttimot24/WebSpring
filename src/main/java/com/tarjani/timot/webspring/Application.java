6e9f5c9161e56337c2df1f08e68cefd277d6e967
Timot Tarjani <ttimotster@gmail.com>
2019-04-30 20:50:08 +0200

    Remove import Swagger

--- src/main/java/com/tarjani/timot/webspring/Application.java
+++ src/main/java/com/tarjani/timot/webspring/Application.java
@@ -6,7 +6,6 @@ import com.tarjani.timot.webspring.config.AuthConfig;
 import com.tarjani.timot.webspring.config.ObjectMapperConfig;
 import com.tarjani.timot.webspring.config.PersistenceConfig;
 import com.tarjani.timot.webspring.config.SecurityInitializer;
-import com.tarjani.timot.webspring.config.SwaggerConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +16,6 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
  *
@@ -42,7 +35,6 @@ public class Application implements WebApplicationInitializer {
         application.register(SecurityInitializer.class);
         application.register(AuthConfig.class);
         application.register(ObjectMapperConfig.class);
-      //  application.register(SwaggerConfig.class);
         
         container.addListener(new ContextLoaderListener(application));
         
