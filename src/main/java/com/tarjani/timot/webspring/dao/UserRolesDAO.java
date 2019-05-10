042594f5cb1b59aba36ee333dcbb8b6696cdc533
Timot Tarjani <ttimotster@gmail.com>
2019-05-10 21:19:08 +0200

    Improve DAOs with getClass()

--- src/main/java/com/tarjani/timot/webspring/dao/UserRolesDAO.java
+++ src/main/java/com/tarjani/timot/webspring/dao/UserRolesDAO.java
@@ -18,7 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserRolesDAO extends AbstractHibernateDAO < UserRole >{
     
     public UserRolesDAO(){
-        this.setEntityClass(UserRole.class);
+        this.setEntityClass(this.getClass());
     }
     
 }
