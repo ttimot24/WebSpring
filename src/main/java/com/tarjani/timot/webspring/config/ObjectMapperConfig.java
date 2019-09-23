96a964c16ebea9ac9d28bd8e9ff108de4d18d9e9
ttimot24 <ttimotster@gmail.com>
2019-09-23 12:04:19 +0200

    Added XMLMapper bean

--- src/main/java/com/tarjani/timot/webspring/config/ObjectMapperConfig.java
+++ src/main/java/com/tarjani/timot/webspring/config/ObjectMapperConfig.java
@@ -8,6 +8,7 @@ package com.tarjani.timot.webspring.config;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +22,16 @@ public class ObjectMapperConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+
+        return mapper;
+    }
+    
+    
+    @Bean
+    public XmlMapper xmlMapper() {
+        XmlMapper mapper = new XmlMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
 
